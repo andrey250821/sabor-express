ad05acf27a25266c73935006d938c75dfcec2f52
Adrian Condori Mita <adriancondori300@gmail.com>
2026-09-11 14:05:57 -0400

    Avance general Sabor Express - seguimiento delivery y mapas

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -396,7 +396,15 @@
     <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
     </script>
+    {{-- Leaflet CSS --}}
+    <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css">
 
+    {{-- CSS principal --}}
+    <link
+        rel="stylesheet"
+        href="{{ asset('css/admin.css') }}">
     {{-- Leaflet JS --}}
     <script
         src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js">
