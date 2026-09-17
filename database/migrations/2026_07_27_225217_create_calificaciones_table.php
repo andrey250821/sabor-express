d5a8db4eca559f6f7a2e561e61868999b8fe3be5
Adrian Condori Mita <adriancondori300@gmail.com>
2026-09-17 15:56:04 -0400

    Corregir estructura de calificaciones

--- database/migrations/2026_07_27_225217_create_calificaciones_table.php
+++ database/migrations/2026_07_27_225217_create_calificaciones_table.php
@@ -4,53 +4,35 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
 return new class extends Migration
 {
-
     public function up(): void
     {
-
         Schema::create('calificaciones', function (Blueprint $table) {
-
-
             $table->id();
 
-
-
             $table->foreignId('pedido_id')
-                  ->constrained('pedidos')
-                  ->cascadeOnDelete();
-
-
+                ->constrained('pedidos')
+                ->cascadeOnDelete();
 
             $table->foreignId('user_id')
-                  ->constrained('users')
-                  ->cascadeOnDelete();
+                ->constrained('users')
+                ->cascadeOnDelete();
 
-
+            $table->foreignId('producto_id')
+                ->constrained('productos')
+                ->cascadeOnDelete();
 
             $table->integer('puntuacion');
 
-
-
-            $table->text('comentario')
-                  ->nullable();
-
-
+            $table->text('comentario')->nullable();
 
             $table->timestamps();
-
-
         });
-
     }
-
-
 
     public function down(): void
     {
         Schema::dropIfExists('calificaciones');
     }
-
 };
