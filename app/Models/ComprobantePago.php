b2ea85f4818ab23bb4483f4019e8509bf2aaf59c
andrey250821 <adriancondori300@gmail.com>
2026-09-18 19:22:03 -0400

    Guardar datos OCR en comprobantes

--- app/Models/ComprobantePago.php
+++ app/Models/ComprobantePago.php
@@ -7,18 +7,25 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ComprobantePago extends Model
 {
-
     protected $table = 'comprobantes_pago';
-
 
     protected $fillable = [
         'pedido_id',
         'imagen',
         'referencia_bancaria',
+        'motivo_revision',
+        'datos_ocr',
         'estado',
         'fecha_revision',
     ];
 
+    protected function casts(): array
+    {
+        return [
+            'datos_ocr' => 'array',
+            'fecha_revision' => 'datetime',
+        ];
+    }
 
     public function pedido(): BelongsTo
     {
