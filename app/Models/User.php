390d9a932bd44aa448eae0a5744a9c3b30437a93
andrey250821 <adriancondori300@gmail.com>
2026-09-22 16:06:02 -0400

    Agregar relación de pedidos por cocinero

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,6 +67,14 @@ class User extends Authenticatable
     }
 
     /**
+     * Pedidos tomados por este cocinero.
+     */
+    public function pedidosCocina(): HasMany
+    {
+        return $this->hasMany(Pedido::class, 'cocinero_id');
+    }
+
+    /**
      * Un delivery puede tener muchas asignaciones.
      */
     public function asignacionesDelivery(): HasMany
